faff3ed91f7d5546e9ffb67d72826f8ee9803715
sakutarou <262035451+saku-day1@users.noreply.github.com>
2026-04-21 11:10:11 +0900

    git commit -m "コンバーターの紐づかなかった場合のテスト"

--- src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
@@ -9,30 +9,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class StudentConverterTest {
     @Test
-    void 受講生IDを元に受講生と受講生コース情報の紐づけが行われること()  {
+    void 受講生IDを元に受講生と受講生コース情報の紐づけが行われること() {
+        //準備
+        //受講生情報とコース情報とコンバーターの準備
+        List<StudentDetail> converterStudentDetails = getStudentDetails("1");
+        assertEquals(1, converterStudentDetails.size());
+
+        StudentDetail actual = converterStudentDetails.get(0);
+
+        assertEquals("1", actual.getStudent().getId());
+        assertEquals(1, actual.getStudentCourseList().size());
+        assertEquals("1", actual.getStudentCourseList().get(0).getStudentId());
+    }
+
+    @Test
+    void 受講生IDが違う場合に受講生と受講生コース情報は紐づかないこと() {
+        List<StudentDetail> converterStudentDetails = getStudentDetails("2");
+
+        assertEquals(1, converterStudentDetails.size());
+
+        StudentDetail actual = converterStudentDetails.get(0);
+
+        assertEquals("1", actual.getStudent().getId());
+        assertTrue(actual.getStudentCourseList().isEmpty());
+    }
+
+    // 受講生IDが"1"の受講生情報と、引数で指定した受講生IDを持つコース情報を作成し、
+    // コンバーターで受講生詳細リストに変換するメソッド
+    private static List<StudentDetail> getStudentDetails(String courseStudentId) {
         Student student = new Student();
         student.setId("1");
         StudentCourse course = new StudentCourse();
-        course.setStudentId("1");
+        course.setStudentId(courseStudentId);
         StudentConverter converter = new StudentConverter();
+
         List<Student> studentList = new ArrayList<>();
         studentList.add(student);
-
         List<StudentCourse> studentCourseList = new ArrayList<>();
         studentCourseList.add(course);
+
         List<StudentDetail> converterStudentDetails =
                 converter.convertStudentDetails(studentList, studentCourseList);
-        assertEquals(1,converterStudentDetails.size());
-
-        StudentDetail actual =converterStudentDetails.get(0);
-
-        assertEquals("1",actual.getStudent().getId());
-        assertEquals(1,actual.getStudentCourseList().size());
-        assertEquals("1",actual.getStudentCourseList().get(0).getStudentId());
+        return converterStudentDetails;
     }
-
 }
