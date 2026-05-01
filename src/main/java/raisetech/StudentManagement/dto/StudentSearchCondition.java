08c14774e785263c53f1d215effe4c7c4681cb03
sakutarou <262035451+saku-day1@users.noreply.github.com>
2026-05-01 15:43:43 +0900

    git commit -m "StudentSearchConditionのバリデーションと不必要な変数の削除"

--- src/main/java/raisetech/StudentManagement/dto/StudentSearchCondition.java
+++ src/main/java/raisetech/StudentManagement/dto/StudentSearchCondition.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.dto;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,15 +14,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentSearchCondition {
+
     private String name;
+
     private String furigana;
+
     private String courseName;
+
+    @Pattern(regexp = "^\\d+$", message = "受講生IDは数字で入力してください")
     private String studentId;
+
+    @Pattern(regexp = "^\\d+$", message = "受講生コースIDは数字で入力してください")
     private String studentCourseId;
+
     private String status;
-    private String email;
-    private String area;
-    private Integer age;
-    private String gender;
-    private Boolean deleted;
 }
