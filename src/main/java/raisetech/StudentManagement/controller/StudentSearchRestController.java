2cec257529a38d11aaa48b7f66fba155ceddec78
sakutarou <262035451+saku-day1@users.noreply.github.com>
2026-05-01 15:42:06 +0900

    git commit -m "検索処理のコントローラークラスの追加とGETMappingのメソッド追加"

---
+++ src/main/java/raisetech/StudentManagement/controller/StudentSearchRestController.java
@@ -0,0 +1,27 @@
+package raisetech.StudentManagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.dto.StudentSearchCondition;
+import raisetech.StudentManagement.dto.StudentSearchSummary;
+import raisetech.StudentManagement.service.StudentSearchService;
+
+import java.util.List;
+
+@RestController
+
+public class StudentSearchRestController {
+    private final StudentSearchService service;
+
+    public StudentSearchRestController(StudentSearchService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/api/students/search")
+    public List<StudentSearchSummary> searchStudentSummaries(
+            @ModelAttribute StudentSearchCondition condition) {
+        return service.searchStudentSummaries(condition);
+    }
+}
+
