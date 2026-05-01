dd1d1fef84c9b3ccc8a49339fd55f892ff3f95d5
sakutarou <262035451+saku-day1@users.noreply.github.com>
2026-05-01 22:50:12 +0900

    git commit -m "各APIのJavaDoc整備"

--- src/main/java/raisetech/StudentManagement/controller/StudentSearchRestController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentSearchRestController.java
@@ -10,6 +10,9 @@ import raisetech.StudentManagement.service.StudentSearchService;
 
 import java.util.List;
 
+/**
+ * 受講生の検索を行うコントローラークラスです
+ */
 @RestController
 
 public class StudentSearchRestController {
@@ -18,11 +21,21 @@ public class StudentSearchRestController {
     public StudentSearchRestController(StudentSearchService service) {
         this.service = service;
     }
-
+    /**
+     * 受講生の検索を行います。
+     *
+     * <p>複数の検索条件を指定した場合はAND条件で検索されます。
+     * nameおよびfuriganaは部分一致、それ以外は完全一致で検索されます。</p>
+     *
+     * <p>検索条件を指定しない場合は全件検索となります。</p>
+     *
+     * @param criteria 検索条件
+     * @return 受講生サマリー情報のリスト
+     */
     @GetMapping("/api/students/search")
     public List<StudentSearchSummary> searchStudentSummaries(
-            @Validated  @ModelAttribute StudentSearchCriteria condition) {
-        return service.searchStudentSummaries(condition);
+            @Validated  @ModelAttribute StudentSearchCriteria criteria) {
+        return service.searchStudentSummaries(criteria);
     }
 }
 
